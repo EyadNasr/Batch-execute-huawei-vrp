a9644e0d3bdbbb24c8811b62f27f6f0d27d461ce
Eyad Nasr <62260537+EyadNasr@users.noreply.github.com>
2024-05-04 15:32:50 +0300

    getInputs.py

--- getInputs.py
+++ getInputs.py
@@ -14,15 +14,7 @@ def getInputs(outputDir, curfilename):
 	credentials = []
 	for info in devices_lst:
 		creds = [dev.split('***') for dev in info.split('\n') if dev != '']
-		if len(devices_lst) == 1: Foldername = 'Output'
-		else:
-			if any(i in str(creds).lower() for i in ['oob','eor','tor','ce']): Foldername = 'Switches'
-			elif 'ne' in str(creds).lower(): Foldername = 'Routers'
-			elif 'fw' in str(creds).lower(): Foldername = 'Firewalls'
-			else: Foldername = 'Output'
+		Foldername = 'Output'
 		os.mkdir(Foldername)
 		credentials += [{'NE_Name': tup[0], 'host': tup[1], "password": tup[2], "username": tup[3], "Foldername": Foldername}  for tup in creds]
 	return credentials
-
-# from autom.getInputs import getInputs
-# credentials = getInputs('Z:\\Ed\\Portable_python\\scripts_Outputs\\', os.path.basename(__file__)[:-3])
